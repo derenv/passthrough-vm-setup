bb7144999915775e3d7dc8a5ae668c8896e41fb2
Deren Vural <derenv@live.co.uk>
2022-09-11 17:19:05 +0100

    modified main script following trunk recommendations
    Signed-off-by: Deren Vural <derenv@live.co.uk>

--- gpu-vm-setup.sh
+++ gpu-vm-setup.sh
@@ -23,24 +23,24 @@ sudo apt autoclean
 sudo apt autoremove
 
 # Utility programs
-if [[ "$UTILITIES" -eq 1 ]]; then
-    #aria2 - modern wget/cURL alternative
-    #git - source control
-    sudo apt install -y git aria2
+if [[ ${UTILITIES} -eq 1 ]]; then
+	#aria2 - modern wget/cURL alternative
+	#git - source control
+	sudo apt install -y git aria2
 
-    # oh-my-bash
-    bash -c "$(aria2c https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
+	# oh-my-bash
+	bash -c "$(aria2c https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)" || true
 fi
 
 # For drivers
 sudo apt install nvidia-driver-515 nvidia-utils-515 nvidia-settings
 sudo nvidia-xconfig # This creates a config file, needed for external monitor
 
-if [[ "$STEAM_FLATPAK" -eq 1 ]]; then
-    # For Steam
-    sudo apt install flatpak gnome-software-plugin-flatpak gnome-software
-    flatpak install com.valvesoftware.Steam
-elif [[ "$STEAM" -eq 1 ]]; then
-    # For Steam
-    sudo apt install steam-installer
+if [[ ${STEAM_FLATPAK} -eq 1 ]]; then
+	# For Steam
+	sudo apt install flatpak gnome-software-plugin-flatpak gnome-software
+	flatpak install com.valvesoftware.Steam
+elif [[ ${STEAM} -eq 1 ]]; then
+	# For Steam
+	sudo apt install steam-installer
 fi
